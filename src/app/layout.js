6bc8c982c64732239b0bd912cf2fbd94c024b432
Safal Neupane <safal.npane@gmail.com>
2023-08-06 23:38:16 +1000

    feat(*): setup nextjs with necessary asset

---
+++ src/app/layout.js
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "Safal Neupane",
+  description: "Full-stack developer",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
